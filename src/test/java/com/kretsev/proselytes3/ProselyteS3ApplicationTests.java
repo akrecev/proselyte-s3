96e46698ebb556bdd77258c10e8d9f0b971442d4
RWEC\akrecev <akretsev@gmail.com>
2023-12-15 15:29:24 +0300

    initial commit

---
+++ src/test/java/com/kretsev/proselytes3/ProselyteS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kretsev.proselytes3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProselyteS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
